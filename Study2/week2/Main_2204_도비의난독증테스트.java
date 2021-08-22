bc2ef53745eece86fd9df1393040375b4d0759ec
방수영 <qtd5079@naver.com>
2021-08-22 20:05:32 +0900

    class명 수정

--- Study2/week2/Main_2204_도비의난독증테스트.java
+++ Study2/week2/Main_2204_도비의난독증테스트.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_2204 {
+public class Main_2204_도비의난독증테스트 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
