22d1eba4c4964ff323edfbfdda78af59f724abc9
방수영 <qtd5079@naver.com>
2021-10-24 23:34:29 +0900

    다시 풀어보기

---
+++ Study2/week8/Main_boj_1138_한줄로서기_방수영.java
@@ -0,0 +1,5 @@
+package week8;
+
+public class Main_boj_1138_한줄로서기_방수영 {
+
+}
---
+++ Study2/week8/Main_boj_1206_사람의수_방수영.java
@@ -0,0 +1,10 @@
+package week8;
+
+public class Main_boj_1206_사람의수_방수영 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
